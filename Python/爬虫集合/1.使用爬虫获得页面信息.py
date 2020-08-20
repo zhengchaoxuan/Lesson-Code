a83b3e10a32131a41e0849956c4f1790388d4dd6
zhengchaoxuan <1139500799@qq.com>
2020-08-20 20:44:03 +0800

    爬取text文件的代码

---
+++ Python/爬虫集合/1.使用爬虫获得页面信息.py
@@ -0,0 +1,51 @@
+import requests
+from bs4 import BeautifulSoup
+
+def get_url(url):
+    #使用反爬机制，模拟使用页面
+    headers = {'User-Agent' : 'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.132 Safari/537.36'}
+    r = requests.get(url,headers = headers)
+    return r.text
+
+def get_content(html,page):
+    output = """第{}页 作者：{} 性别：{} 年龄：{} 点赞：{} 评论：{}\n{}\n------------\n""" # 最终输出格式
+    Soup = BeautifulSoup(html,'html.parser') #进行语法剖析
+    Con = Soup.find(class_='content-block clearfix')  # 如图一红色方框
+    Content_list = Con.find_all('div',class_ = 'article')
+    for i in Content_list:
+        name = i.find('h2').string
+        author_info = i.find('div',class_ = 'articleGender')
+        if author_info is not None:
+            if  'womenIco' == author_info['class']:
+                sex = '女'
+                
+            elif 'menIco' == author_info['class']:
+                sex = '男'
+            else:
+                sex = ' '
+            age = author_info.string
+        else:
+            sex = ' '
+            age = ' '
+
+        Smiles = i.find('div',class_ = 'stats').find(class_='stats-vote').find('i',class_='number').string
+        Comment = i.find('div',class_ = 'stats').find(class_='stats-comments').find('i',class_='number').string
+        Content =i.find('div',class_ = 'content').find('span').get_text()
+
+        write_txt(output.format(page,name,sex,age,Smiles,Comment,Content))
+
+def write_txt(*args):
+    for i in args:
+        with open('output.txt','a',encoding='utf-8') as f:
+             f.write(i)        
+
+def main():
+    for i in range(1,11):
+        #获得1-10页的URL
+        url = "https://www.qiushibaike.com/text/page/{}".format(i)
+        Html = get_url(url)
+        #解析页面
+        get_content(Html,i)
+
+if __name__ == "__main__":
+    main()
